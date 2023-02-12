7c5fe5e4ca13ce725af3186f9432c3a8a904697b
Duminda Kuruppu <kuruppu.duminda@gmail.com>
2023-02-12 21:47:41 +0530

    added media player

--- app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
@@ -1,5 +1,7 @@
 package com.example.a7minuteworkout
 
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -26,6 +28,7 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private var currentExercisePosition = -1
 
     private var textToSpeechObject: TextToSpeech? = null
+    private var player: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -89,6 +92,15 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private fun setupRestView() {
 
+        try {
+            val soundURI = Uri.parse("android.resource://com.example.a7minuteworkout/raw/" + R.raw.press_start)
+            player = MediaPlayer.create(applicationContext, soundURI)
+            player?.isLooping = false
+            player?.start()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+
         binding?.flRestView?.visibility = View.VISIBLE
         binding?.flExerciseView?.visibility = View.INVISIBLE
         binding?.textViewTitle?.visibility = View.VISIBLE
@@ -141,6 +153,10 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         if (textToSpeechObject != null) {
             textToSpeechObject!!.stop()
             textToSpeechObject!!.shutdown()
+        }
+
+        if (player != null) {
+            player!!.stop()
         }
 
         binding = null
